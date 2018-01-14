7a81d64cc90ba0bbea12dbb369c5ae3f65be7dca
Jon Chui <jon@lpcuwc.edu.hk>
2018-01-14 17:13:55 +0800

    functional query and mutation for number

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template lang="pug">
-  h1 {{ Topic }}
+  div
+    //- p {{ Topic }}
+    hr
+    //- p {{ Subtopic }}
+    hr
+    p {{ allTesting123s }}
+    hr
+    input(v-model="userInput" type="number")
+    hr
+    button(@click="createNewTesting" label="click") Huzah!
+
 </template>
 
 <script>
@@ -23,7 +33,49 @@ const Topic = gql`
       }
     }
   }
+`
+const Subtopic = gql`
+  query Subtopic($ID: String!, $includeTopic: Boolean!) {
+    Subtopic(subtopicID: $ID) {
+      subtopicID
+      translations {
+        title
+        essentialIdea
+      }
+      hours
+      topic @include(if: $includeTopic) {
+        id,
+        topicIDNumber,
+        translations {
+          title
+        }
+      }
+    }
+  }
+`
 
+const subtopicInput = {
+}
+
+const allTesting123s = gql`
+  query {
+    allTesting123s {
+      id,
+      someNumberField
+    }
+  }
+`
+const CREATE_NEW_TESTING123 = gql`
+mutation createTest($num: Int!) {
+  createTesting123(
+    someNumberField: $num,
+    isPublished: true
+  ) {
+    id
+    someNumberField
+    isPublished
+  }
+}
 `
 
 export default {
@@ -31,48 +83,47 @@ export default {
   data () {
     return {
       msg: 'try',
-      loading: 0
+      loading: 0,
+      userInput: 0
+    }
+  },
+  methods: {
+    createNewTesting () {
+      this.$apollo.mutate({
+        mutation: CREATE_NEW_TESTING123,
+        variables: {
+          num: parseInt(this.userInput)
+        }
+      })
+
+      console.log('num added')
     }
   },
   apollo: {
     $loadingKey: 'loading',
-    Topic: {
-      query: Topic,
-      variables: {
-        skip: 0,
-        ID: "1",
-        includeSubtopics: true
-      }
+    // Topic: {
+    //   query: Topic,
+    //   variables: {
+    //     // skip: 0,
+    //     ID: "1",
+    //     includeSubtopics: true
+    //   }
+    // },
+    // Subtopic: {
+    //   query: Subtopic,
+    //   variables: {
+    //     // skip: 0,
+    //     ID: "1.1",
+    //     includeTopic: true
+    //     // includeSubSubtopics: true
+    //   }
+    // }
+    allTesting123s: { 
+      query: allTesting123s
     }
   },
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
